59db4ad98af32aada6825452f5fe1a38b77cbb64
Adam Bazzi <adambazzi2416@gmail.com>
2020-08-23 19:57:08 -0400

    Added animation to hamburger menu fade in and out

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,11 @@
 function hideNavItems(){
     var navItems = document.getElementById("navItems");
     if(navItems.classList.contains('md:hidden')){
+        navItems.classList.add('animate__animated', 'animate__fadeIn')
         navItems.classList.remove('md:hidden');
     } else {
+        navItems.classList.remove('animate__animated', 'animate__fadeIn');
+        navItems.classList.add('animate__animated', 'animate__fadeIn');
         navItems.classList.add('md:hidden');
     }
   }
