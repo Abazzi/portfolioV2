70c23532800cf7257815466e8be9e35ae9d22b4c
Adam Bazzi <adambazzi2416@gmail.com>
2020-08-23 10:51:36 -0400

    Added New Fonts added Nav bar text (Possible change if I come up with something better)

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          100: '#5F5B57'
+        },
+
+        pink: {
+          100: '#D17A91'
+        },
+
+        white: {
+          100: '#E7E2DE'
+        }
+      },
+
+      fontFamily: {
+        'code': ['Inconsolata', 'sans-serif'],
+        'body': ['"Didact Gothic"', 'sans-serif']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
