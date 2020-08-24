ac15bd9e73be77adae12a8b86cc3c545ebea7e47
Adam Bazzi <adambazzi2416@gmail.com>
2020-08-24 11:26:33 -0400

    Changed form Flexbox to CSS Grid as it suits my needs better #1

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 function hideNavItems(){
     var navItems = document.getElementById("navItems");
     if(navItems.classList.contains('md:hidden')){
-        navItems.classList.add('animate__animated', 'animate__fadeIn');
+        if(navItems.classList.contains('animate__fadeOut')){
+            navItems.classList.replace('animate__fadeOut', 'animate__fadeIn');
+        }
         navItems.classList.remove('md:hidden');
     } else {
         navItems.classList.replace('animate__fadeIn', 'animate__fadeOut');
