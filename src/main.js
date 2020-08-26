c2d462649d21360e5857b2331427399986606f99
Adam Bazzi <adambazzi2416@gmail.com>
2020-08-26 19:32:22 -0400

    Added Menu buttons with proper positioning #1 #2 Changed animation to a more subtle one

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 function hideNavItems(){
     var navItems = document.getElementById("navItems");
         if(navItems.classList.contains('md:hidden')){
-            if(navItems.classList.contains('animate__backOutDown')){
-                navItems.classList.replace('animate__backOutDown', 'animate__backOutUp');
+            if(navItems.classList.contains('animate__zoomOut')){
+                navItems.classList.replace('animate__zoomOut', 'animate__zoomIn');
             }
             navItems.classList.remove('md:hidden');
         } else {
-            navItems.classList.replace('animate__backOutUp', 'animate__backOutDown');
-            navItems.classList.add('hidden');
+            if(navItems.classList.contains('animate__zoomIn')){
+                navItems.classList.replace('animate__zoomIn', 'animate__zoomOut');
+            }
+                navItems.classList.add('md:hidden');
         }
     }
