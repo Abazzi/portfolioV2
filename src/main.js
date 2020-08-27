a43d668dcd8a842c7c0f81b55230b189a5914985
Adam Bazzi <adambazzi2416@gmail.com>
2020-08-27 12:02:17 -0400

    Removed Animate.css because it's overkill for what I need Adjusted the nav bar for mobile and the fab button for desktop

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
 function hideNavItems(){
     var navItems = document.getElementById("navItems");
         if(navItems.classList.contains('md:hidden')){
-            if(navItems.classList.contains('animate__zoomOut')){
-                navItems.classList.replace('animate__zoomOut', 'animate__zoomIn');
-            }
             navItems.classList.remove('md:hidden');
         } else {
-            if(navItems.classList.contains('animate__zoomIn')){
-                navItems.classList.replace('animate__zoomIn', 'animate__zoomOut');
-            }
-                navItems.classList.add('md:hidden');
+            navItems.classList.add('md:hidden');
         }
     }
