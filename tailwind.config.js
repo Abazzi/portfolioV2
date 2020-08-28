0936ce9230ef6f3a30f4441bb495419a17eae7de
Adam Bazzi <adambazzi2416@gmail.com>
2020-08-28 16:33:56 -0400

    Removed all fonts and added B612 font

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,7 +38,7 @@ module.exports = {
 
       fontFamily: {
         'code': ['Inconsolata', 'sans-serif'],
-        'body': ['C800', 'sans-serif'],
+        'body': ['"B612"', 'sans-serif'],
       },
 
       rotate: {
